b2ecb5a6b40c295a4fa627b68becacce8253ba6f
Mickael Albisser <mickael.albisser@slatedigital.com>
2023-12-07 21:56:22 +0100

    Cleanup day 5 solution

--- day_5_part_2.py
+++ day_5_part_2.py
@@ -1,5 +1,4 @@
 import re
-from math import inf
 from copy import copy
 
 from day_5_input import value as puzzle_input
@@ -9,14 +8,7 @@ lines = puzzle_input.splitlines()
 seeds = [int(seed) for seed in re.findall("\d+", lines[0])]
 seeds_ranges = []
 for i in range(len(seeds) // 2):
-    seeds_ranges.append([seeds[2*i], seeds[2*i+1]])
-
-
-def custom_sort(lst):
-    return sorted(lst, key=lambda elem: elem[0])
-
-
-seeds_ranges = custom_sort(seeds_ranges)
+    seeds_ranges.append([seeds[2 * i], seeds[2 * i + 1]])
 
 
 current_map = None
@@ -50,14 +42,6 @@ for line in lines:
         target_range, source_range, range_length = [int(match) for match in regex_match]
         current_map.append((source_range, range_length, target_range))
 
-seed_soil_map = custom_sort(seed_soil_map)
-soil_fertilizer_map = custom_sort(soil_fertilizer_map)
-fertilizer_water_map = custom_sort(fertilizer_water_map)
-water_light_map = custom_sort(water_light_map)
-light_temperature_map = custom_sort(light_temperature_map)
-temperature_humidity_map = custom_sort(temperature_humidity_map)
-humidity_location_map = custom_sort(humidity_location_map)
-
 
 def map_ranges(input_ranges, maps):
     next_iteration_ranges = input_ranges
@@ -66,9 +50,6 @@ def map_ranges(input_ranges, maps):
         current_iteration_ranges = copy(next_iteration_ranges)
         next_iteration_ranges = []
         for range_index, current_range in enumerate(current_iteration_ranges):
-            # current_range_tmp = copy(current_range)
-            # low_bound = max(current_range[0], current_map[0])
-            # high_bound = min(sum(current_range), sum(current_map[0:2]))
             range_end = sum(current_range)
             map_end = sum(current_map[0:2])
             pre_length = max(0, min(range_end, current_map[0]) - current_range[0])
@@ -81,17 +62,6 @@ def map_ranges(input_ranges, maps):
             if effect_length > 0:
                 output_start = current_map[2] - current_map[0] + current_range[0] + pre_length
                 output_ranges.append([output_start, effect_length])
-
-            # if high_bound > low_bound:
-            #     if low_bound == current_range[0] and high_bound == sum(current_range):
-            #         input_ranges.pop(range_index)
-            #         index_pad = 0
-            #     else:
-            #         current_range[1] = low_bound - current_range[0]
-            #         index_pad = 1
-            #     input_ranges.insert(range_index + index_pad, [low_bound, high_bound - low_bound])
-            #     if high_bound < sum(current_range_tmp):
-            #         input_ranges.insert(range_index + index_pad + 1, [high_bound, high_bound - current_range_tmp[1]])
     return output_ranges + next_iteration_ranges
 
 
@@ -106,6 +76,7 @@ def get_locations(seeds_ranges):
     return locations
 
 
-output_ranges = custom_sort(get_locations(seeds_ranges))
+output_ranges = get_locations(seeds_ranges)
+output_ranges.sort(key=lambda elem: elem[0])
 
 print(output_ranges[0][0])
