86c9d7607cf5739a022a40c4fc09e5f57eb0e336
Mickael Albisser <mickael.albisser@slatedigital.com>
2023-12-07 21:52:49 +0100

    Functional day 5 solution

--- day_5_part_2.py
+++ day_5_part_2.py
@@ -2,7 +2,7 @@ import re
 from math import inf
 from copy import copy
 
-from day_5_input_simple import value as puzzle_input
+from day_5_input import value as puzzle_input
 
 lines = puzzle_input.splitlines()
 
@@ -59,21 +59,29 @@ temperature_humidity_map = custom_sort(temperature_humidity_map)
 humidity_location_map = custom_sort(humidity_location_map)
 
 
-def map_ranges(input_ranges, map):
+def map_ranges(input_ranges, maps):
+    next_iteration_ranges = input_ranges
     output_ranges = []
-    for range_index, current_range in enumerate(input_ranges):
-        for current_map in map:
-            current_range_tmp = copy(current_range)
-            low_bound = max(current_range[0], current_map[0])
-            high_bound = min(sum(current_range), sum(current_map[0:2]))
-            pre_length = max(low_bound - current_range[0], 0)
-            post_length = max(high_bound - sum(current_range), 0)
+    for current_map in maps:
+        current_iteration_ranges = copy(next_iteration_ranges)
+        next_iteration_ranges = []
+        for range_index, current_range in enumerate(current_iteration_ranges):
+            # current_range_tmp = copy(current_range)
+            # low_bound = max(current_range[0], current_map[0])
+            # high_bound = min(sum(current_range), sum(current_map[0:2]))
+            range_end = sum(current_range)
+            map_end = sum(current_map[0:2])
+            pre_length = max(0, min(range_end, current_map[0]) - current_range[0])
+            effect_length = max(0, min(map_end, range_end) - max(current_map[0], current_range[0]))
+            post_length = max(0, range_end - max(map_end, current_range[0]))
             if pre_length > 0:
-                output_ranges.append([current_range[0], pre_length])
+                next_iteration_ranges.append([current_range[0], pre_length])
             if post_length > 0:
-                output_ranges.append([high_bound, post_length])
-            if high_bound > low_bound:
-                output_ranges.append([current_map[2], high_bound - low_bound])
+                next_iteration_ranges.append([current_range[0] + pre_length + effect_length, post_length])
+            if effect_length > 0:
+                output_start = current_map[2] - current_map[0] + current_range[0] + pre_length
+                output_ranges.append([output_start, effect_length])
+
             # if high_bound > low_bound:
             #     if low_bound == current_range[0] and high_bound == sum(current_range):
             #         input_ranges.pop(range_index)
@@ -84,20 +92,20 @@ def map_ranges(input_ranges, map):
             #     input_ranges.insert(range_index + index_pad, [low_bound, high_bound - low_bound])
             #     if high_bound < sum(current_range_tmp):
             #         input_ranges.insert(range_index + index_pad + 1, [high_bound, high_bound - current_range_tmp[1]])
-    return output_ranges
+    return output_ranges + next_iteration_ranges
 
 
-# def get_location(seed):
-#     soil = map_value(seed, seed_soil_map)
-#     fertilizer = map_value(soil, soil_fertilizer_map)
-#     water = map_value(fertilizer, fertilizer_water_map)
-#     light = map_value(water, water_light_map)
-#     temperature = map_value(light, light_temperature_map)
-#     humidity = map_value(temperature, temperature_humidity_map)
-#     location = map_value(humidity, humidity_location_map)
-#     return location
+def get_locations(seeds_ranges):
+    soils = map_ranges(seeds_ranges, seed_soil_map)
+    fertilizers = map_ranges(soils, soil_fertilizer_map)
+    waters = map_ranges(fertilizers, fertilizer_water_map)
+    lights = map_ranges(waters, water_light_map)
+    temperatures = map_ranges(lights, light_temperature_map)
+    humidities = map_ranges(temperatures, temperature_humidity_map)
+    locations = map_ranges(humidities, humidity_location_map)
+    return locations
 
-result = inf
-output_ranges = map_ranges(seeds_ranges, seed_soil_map)
 
-print(result)
+output_ranges = custom_sort(get_locations(seeds_ranges))
+
+print(output_ranges[0][0])
