97012e895f6a03b7aaf07a5afb87140ed94d16f5
Mickael Albisser <mickael.albisser@slatedigital.com>
2023-12-27 10:04:54 +0100

    WIP on day 25

---
+++ day_25/part_1.py
@@ -0,0 +1,48 @@
+import itertools
+from collections import defaultdict
+
+with open("puzzle_input.txt") as input_file:
+    puzzle_input = input_file.read()
+
+nodes = defaultdict(list)
+
+for line in puzzle_input.splitlines():
+    src, destinations = line.split(":")
+    destinations = destinations.split()
+    for dest in destinations:
+        if dest not in nodes[src]:
+            nodes[src].append(dest)
+        if src not in nodes[dest]:
+            nodes[dest].append(src)
+
+
+def get_shortest_path(begin, end):
+    seen = set(begin)
+    current_nodes = [begin]
+    visited_nodes = [[begin]]
+
+    while end not in seen:
+        next_nodes = []
+        next_visited_nodes = []
+        for i, node in enumerate(current_nodes):
+            for destination in nodes[node]:
+                if destination not in seen:
+                    next_nodes.append(destination)
+                    next_visited_nodes.append(visited_nodes[i] + [destination])
+                    seen.add(destination)
+        current_nodes = next_nodes
+        visited_nodes = next_visited_nodes
+
+    return visited_nodes[current_nodes.index(end)]
+
+
+vertices_count = defaultdict(int)
+for node_a, node_b in itertools.pairwise(nodes.keys()):
+    visited_nodes = get_shortest_path(node_a, node_b)
+    for va, vb in itertools.pairwise(visited_nodes):
+        vertices = [va, vb]
+        vertices.sort()
+        vertices_count[tuple(vertices)] += 1
+vertices_count = {v: c for v, c in sorted(vertices_count.items(), key=lambda item: item[1], reverse=True)}
+
+
